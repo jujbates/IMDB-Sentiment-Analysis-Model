09013d4bb9629ab01890d612640bde025d7cd37a
EC2 Default User <ec2-user@ip-172-16-85-52.us-west-2.compute.internal>
2019-01-17 17:19:59 +0000

    deploy the final model, set up web app for model use, and finally deploy web app

--- serve/predict.py
+++ serve/predict.py
@@ -50,6 +50,7 @@ def input_fn(serialized_input_data, content_type):
     print('Deserializing the input data.')
     if content_type == 'text/plain':
         data = serialized_input_data.decode('utf-8')
+        print('data: ', data)
         return data
     raise Exception('Requested unsupported ContentType in content_type: ' + content_type)
 
@@ -69,10 +70,9 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
-
+    
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
+    
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
@@ -86,7 +86,9 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
+    with torch.no_grad():
+        output = model.forward(data)
 
-    result = None
+    result = np.round(output.numpy())
 
     return result
